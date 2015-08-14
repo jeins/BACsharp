f9b18c9def1ebd51f595f4a46be3b0c0bb43f03d
Muhammad Juan Akbar <akbar@kieback-peter.de>
2015-08-14 12:19:45 +0200

    add FindDeviceProperties

--- AppTest1/BACnet/IBACnetScout.cs
+++ AppTest1/BACnet/IBACnetScout.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 
 namespace AppTest1.BACnet
@@ -35,11 +36,16 @@ namespace AppTest1.BACnet
         public int ProtocolVersion;
 
         public int ProtocolRevision;
+
+        public byte SourceLength;
+
+        public int Network;
     }
 
     public class BACnetIpDevice : BACnetDevice
     {
-        public string IpAddress;
+        public IPEndPoint IpAddress;
+
     }
 
 
@@ -58,51 +64,51 @@ namespace AppTest1.BACnet
         /// Find all BACnet devices in a given IP network.
         /// </summary>
         /// <param name="ipNetwork">The IP network address in CIDR format.</param>
-        /// <param name="port">The UDP port used for BACnet communication.</param>
         /// <remarks>
         /// BACnet devices should return an I-Am when requested by a Who-Is (with no device instance range).
         /// Use unicast Who-Is service as we're not registered at a BBMD in the network.
         /// Reading the required Device properties can be accomplished with a single ReadPropertyMultiple request.
         /// </remarks>
         /// <returns>A list of all found BACnet IP devices.</returns>
-        List<BACnetIpDevice> FindBACnetDevices(string ipNetwork, int port = 47808);
-
+        List<BACnetIpDevice> FindBACnetDevices(IPEndPoint IpAddress);
 
         /// <summary>
+        /// Finds the device properties.
+        /// </summary>
+        /// <returns></returns>
+        List<BACnetDevice> FindDeviceProperties();
+
+            /// <summary>
         /// Find all BACnet devices with enabled BBMD functionality.
         /// </summary>
         /// <param name="ipNetwork">The IP network address in CIDR format.</param>
-        /// <param name="port">The UDP port used for BACnet communication.</param>
         /// <remarks>
         /// Devices with enabled BBMD should ACK ReadBroadcastDistributionTable requests. 
         /// BBMDs with enabled FD registration should ACK ReadForeignDeviceTable requests.
         /// </remarks>
         /// <returns></returns>
-        List<BACnetDeviceWithBBMD> FindBACnetBBMDs(string ipNetwork, int port = 47808);
+        List<BACnetDeviceWithBBMD> FindBACnetBBMDs(IPEndPoint IpAddress);
 
         /// <summary>
         /// Determines whether there is a BACnet device at the specified IP address and port.
         /// </summary>
         /// <param name="ipAddress">The ip address.</param>
-        /// <param name="port">The BACnet port.</param>
         /// <returns>The BACnet Device Instance Number or 4194303 (BACnet "null") if not a BACnet device.</returns>
-        int IsBACnetIpDevice(string ipAddress, int port = 47808);
+        int IsBACnetIpDevice(IPEndPoint IpAddress);
 
         /// <summary>
         /// Determines whether the BBMD is enabled.
         /// </summary>
         /// <param name="ipAddress">The IP address of a BACnet device.</param>
-        /// <param name="port">The BACnet port.</param>
         /// <returns>True if BBMD is enabled, false otherwise.</returns>
-        bool IsBbmdEnabled(string ipAddress, int port = 47808);
+        bool IsBbmdEnabled(IPEndPoint IpAddress);
 
         /// <summary>
         /// Determines whether the BBMD option "Foreign Device Registration" is enabled on a given BACnet BBMD.
         /// </summary>
         /// <param name="ipAddress">The IP address of a BACnet BBMD.</param>
-        /// <param name="port">The BACnet port.</param>
         /// <returns>True if FD registration is enabled, false otherwise.</returns>
-        bool IsFdRegistrationSupported(string ipAddress, int port = 47808);
+        bool IsFdRegistrationSupported(IPEndPoint IpAddress);
 
     }
 }
